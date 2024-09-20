9a548cb09b3b23b9d6a039f6bd404dff5ed4f72b
abrotech001 <abrotexh@googlemail.com>
2024-09-20 14:46:02 +0100

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
